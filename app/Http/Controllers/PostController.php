ad9559132fdf49b382c8fa9f4c243514ba488bc5
futo11 <futo114031@gmail.com>
2023-10-04 12:32:32 +0900

    投稿機能を追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
+use App\Models\Region;
 
 class PostController extends Controller
 {
@@ -11,6 +12,41 @@ class PostController extends Controller
     {
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+    }
+
+    public function create(Region $region)
+    {
+        return view('posts.create')->with(['regions' => $region->get()]);
+    }
+
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
+    }
+    
     
 }
-
---
+++ database/seeders/RegionSeeder.php
@@ -0,0 +1,256 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+class RegionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('regions')->insert([
+                'name' => '北海道',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+
+        DB::table('regions')->insert([
+                'name' => '青森',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '秋田',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '岩手',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '山形',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '宮城県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '福島県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '新潟県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '富山県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '石川県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '長野県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '群馬県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '栃木県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '茨城県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '千葉県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '埼玉県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '東京都',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '神奈川県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '山梨県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '静岡県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '青森',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '愛知県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '岐阜県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '三重県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '滋賀県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '京都府',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '大阪府',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '奈良県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '和歌山県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '兵庫県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '鳥取県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '島根県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '岡山県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '広島県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '山口県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '香川県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '徳島県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '高知県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '愛媛県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '福岡県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '大分県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '宮崎県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '熊本県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '鹿児島県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '佐賀県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '長崎県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+        DB::table('regions')->insert([
+                'name' => '沖縄県',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+    }
+}
