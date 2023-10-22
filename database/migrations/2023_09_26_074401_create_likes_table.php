aec4723b5ad6465ef1de5ad19b89a047f7a47e8c
futo11 <futo114031@gmail.com>
2023-10-22 10:04:21 +0900

    いいね機能を追加

--- database/migrations/2023_09_26_074401_create_likes_table.php
+++ database/migrations/2023_09_26_074401_create_likes_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
         $table->id();
-        $table->foreignId('post_id')->onDelete('cascade');
-        $table->foreignId('user_id')->onDelete('cascade');
+        $table->unsignedBigInteger('post_id');
+        $table->unsignedBigInteger('user_id');
         $table->timestamps();
         });
     }
